b7b6c9ec686e574583f844a44fff6288a82ab985
gg110402 <giuliagiudici110402@gmail.com>
2024-07-23 15:30:04 +0200

    comitt

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,9 +1,4 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv'; // Aggiunto per caricare le variabili d'ambiente
-
-dotenv.config(); // Aggiunto per caricare il file .env
-
-console.log(process.env.JWT_SECRET)
 
 //creo token jwt
 export const generateJWT = (payload) => {
